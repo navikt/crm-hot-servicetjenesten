09e3e3836800369f954423dfedef3449baf13e94
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2024-11-19 08:41:17 +0100

    Oppdaterer pakkeversjoner

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -1,8 +1,8 @@
 echo "Hei på deg. Oppretter scratch org for deg"
 call sf org create scratch --definition-file config\project-scratch-def.json --alias %1 --duration-days %2 --set-default --json --wait 30
 
-echo "Installerer crm-platform-base ver. 0.235"
-call sf package install --package 04tKB000000Y5CqYAK --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+echo "Installerer crm-platform-base ver. 0.236"
+call sf package install --package 04tKB000000Y5EIYA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
 
 echo "Installerer crm-shared-flowComponents ver. 0.4"
 call sf package install --package 04t7U0000008qz4QAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
