b0612ae50c005f5779a8b33ca828f4fadab777c4
Mathias <mathias.boylestad.kristensen@nav.no>
2025-01-29 09:06:49 +0100

    bytte til lightning modal, mye bedre

--- force-app/main/default/lwc/hot_hotsak/hot_hotsak.js
+++ force-app/main/default/lwc/hot_hotsak/hot_hotsak.js
@@ -3,25 +3,39 @@ import getSaker from '@salesforce/apex/HOT_HotsakIntegrationController.getSaker'
 
 export default class Hot_hotsak extends LightningElement {
     fnrValue = '15084300133';
-    @track saker; // data
-    @track error; // store error object or message
-    @track isLoading = true; // start true until data/error returns
+    @track saker;
+    @track error;
+    @track isLoading = true;
+
+    columns = [
+        { label: 'Mottatt dato', fieldName: 'opprettet', type: 'date' },
+        { label: 'Beskrivelse', fieldName: 'beskrivelse', type: 'text' },
+        { label: 'Status', fieldName: 'saksstatus', type: 'text' },
+        { label: 'Behandlet dato', fieldName: 'endret', type: 'date' },
+        { label: 'Saksid', fieldName: 'saksid', type: 'text' }
+    ];
 
     @wire(getSaker, { fnr: '$fnrValue' })
     wiredSaker({ data, error }) {
-        // Once the wire returns, we're no longer "loading"
         this.isLoading = false;
 
         if (data) {
-            this.error = undefined; // clear any old error
-            this.saker = data;      // store data
+            this.error = undefined;
+
+            // må flytte beskrivelse/gjelder til top level for at det skal fungere med lightning table
+            this.saker = data.map((record) => {
+                return {
+                    ...record,
+                    beskrivelse:
+                        record.behovsmelding && record.behovsmelding.gjelder ? record.behovsmelding.gjelder : ''
+                };
+            });
         } else if (error) {
-            this.saker = undefined; // clear data
-            this.error = error;     // store error info
+            this.saker = undefined;
+            this.error = error;
         }
     }
 
-    // Optional getters for clarity in the template
     get hasData() {
         return !this.isLoading && this.saker && this.saker.length > 0;
     }
