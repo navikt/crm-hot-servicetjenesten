141879b75128d0c7685c003ab189d76396797084
Rasmus Steine Olsen <rasmus.steine.olsen@nav.no>
2025-02-03 06:32:29 +0100

    Handle button click og metode som lager url

--- force-app/main/default/lwc/hot_personHighlightPanelBot/hot_personHighlightPanelBot.js
+++ force-app/main/default/lwc/hot_personHighlightPanelBot/hot_personHighlightPanelBot.js
@@ -7,7 +7,23 @@ export default class hot_personHighlightPanelBot extends LightningElement {
     fnr;
 
     get isDisabledButtons() {
-        // this.personDetails.actorId this.personDetails.personIdent
-        return true;
+        return this.personDetails.personIdent == null;
+    }
+
+    handleButtonClick(event) {
+        var fagsystem = event.currentTarget.dataset.id;
+        var url = this.generateUrl(fagsystem);
+        console.log(url);
+
+        //gå til url her
+        // window.open(url);
+    }
+    generateUrl(fagsystem) {
+        switch (fagsystem) {
+            case 'gosys':
+                return `https://gosys.intern.nav.no/gosys/personoversikt/fnr=${this.personDetails.personIdent}`;
+            default:
+                return null;
+        }
     }
 }
