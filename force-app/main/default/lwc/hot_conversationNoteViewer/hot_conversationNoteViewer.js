1aa9a22762852810fd36173332266ac1c521cc77
Mathias <mathias.boylestad.kristensen@nav.no>
2025-02-20 12:08:44 +0100

    legge til hot_conversationNoteViewer og HOT_Timeline_name på conversationnote

--- force-app/main/default/lwc/hot_conversationNoteViewer/hot_conversationNoteViewer.js
+++ force-app/main/default/lwc/hot_conversationNoteViewer/hot_conversationNoteViewer.js
@@ -1,7 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 import CONVERSATION_NOTE_FIELD from '@salesforce/schema/Conversation_Note__c.CRM_Conversation_Note__c';
-import TIMELINE_NAME_FIELD from '@salesforce/schema/Conversation_Note__c.NKS_Timeline_Name__c';
+import TIMELINE_NAME_FIELD from '@salesforce/schema/Conversation_Note__c.HOT_Timeline_Name__c';
 
 const CONVERSATION_NOTE_FIELDS = [CONVERSATION_NOTE_FIELD, TIMELINE_NAME_FIELD];
 
@@ -9,27 +9,26 @@ export default class Hot_conversationNoteViewer extends LightningElement {
     @api recordId;
     @api objectApiName;
 
-    conversationNote;
+    conversationNote = "heia lyn";
     timelineName;
 
     @wire(getRecord, { recordId: '$recordId', fields: CONVERSATION_NOTE_FIELDS })
     wiredRecord({ error, data }) {
         if (data) {
-            this.conversationNote = getFieldValue(data, CONVERSATION_NOTE_FIELD);
+            //this.conversationNote = getFieldValue(data, CONVERSATION_NOTE_FIELD);
             this.timelineName = getFieldValue(data, TIMELINE_NAME_FIELD);
         } else if (error) {
             console.error('Error fetching Conversation Note:', error);
         }
     }
 
-    //endre navn her når flow blir laget
     handleJournal() {
-        this.startFlow('NKS_Conversation_Note_Journal_Case_v_2');
+        this.startFlow('HOT_Conversation_Note_Journal_Case');
     }
 
-    //endre navn her også
+    //endre navn her hvis navn på flow ikke er korrekt
     handleSetToRedaction() {
-        this.startFlow('NKS_Conversation_Note_Set_Redaction_v_2');
+        this.startFlow('HOT_Conversation_Note_Set_Redaction');
     }
 
     handleCreateNavTask() {
