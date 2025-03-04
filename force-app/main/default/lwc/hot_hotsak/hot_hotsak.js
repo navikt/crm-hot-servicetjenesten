c8d90d52fe50415334b46820896265d83150d5f3
Mathias <33656177+MathiBK@users.noreply.github.com>
2025-03-04 14:36:25 +0100

    TOLK-2763-Hotsak lwc tabell (#176)
    * lagge til basis oppsett av spørring og lwc visning av resultat
    
    * fikse kolonne feil
    
    * fikse dato felt, legge til loading og feilmeldinger
    
    * legge til hotsak fane på pagelayout
    
    * legge til HOT_HotdskIntegrationController til permset
    
    * legge til testklasse
    
    * bytte til lightning modal, mye bedre
    
    * Fikse test
    
    * Delete hot_hotsak.test.js
    
    * endre navn i xml
    
    * legge til henting av fnr basert på hvilket objekt den ligger på. + kommentarer på norsk
    
    * Fikse test klasse og endre kommentarer
    
    * prittier
    
    * legge til på record page etter merge conflict
    
    * flexipage endring
    
    * fikse flexipage feil
    
    * prettier
    
    * legge til console error
    
    * legge til admin permission og fikse comment

---
+++ force-app/main/default/lwc/hot_hotsak/hot_hotsak.js
@@ -0,0 +1,48 @@
+import { LightningElement, wire, track, api } from 'lwc';
+import getCases from '@salesforce/apex/HOT_HotsakIntegrationController.getCases';
+
+export default class Hot_hotsak extends LightningElement {
+    fnrValue = '15084300133';
+    @track saker;
+    @track error;
+    @track isLoading = true;
+    @api objectApiName;
+    @api recordId;
+
+    columns = [
+        { label: 'Mottatt dato', fieldName: 'opprettet', type: 'date' },
+        { label: 'Beskrivelse', fieldName: 'beskrivelse', type: 'text' },
+        { label: 'Status', fieldName: 'saksstatus', type: 'text' },
+        { label: 'Behandlet dato', fieldName: 'endret', type: 'date' },
+        { label: 'Saksid', fieldName: 'saksid', type: 'text' }
+    ];
+
+    @wire(getCases, { recordId: '$recordId', objectApiName: '$objectApiName' })
+    wiredSaker({ data, error }) {
+        this.isLoading = false;
+
+        if (data) {
+            this.error = undefined;
+
+            // Flytter beskrivelse/gjelder til top level for at det skal fungere med lightning table
+            this.saker = data.map((record) => {
+                return {
+                    ...record,
+                    beskrivelse:
+                        record.behovsmelding && record.behovsmelding.gjelder ? record.behovsmelding.gjelder : ''
+                };
+            });
+        } else if (error) {
+            this.saker = undefined;
+            this.error = error;
+            console.error(error);
+        }
+    }
+
+    get hasData() {
+        return !this.isLoading && this.saker && this.saker.length > 0;
+    }
+    get hasNoData() {
+        return !this.isLoading && this.saker && this.saker.length === 0;
+    }
+}
