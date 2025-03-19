2a9d84b358e95892d142959d636f7ba3b20cca3a
Mathias <mathias.boylestad.kristensen@nav.no>
2025-03-19 13:26:26 +0100

    legge til hot versjon av ChatAuthenticated lwc

---
+++ force-app/main/default/lwc/hot_ChatAuthenticated/hot_ChatAuthenticated.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+import setStatusCompleted from '@salesforce/apex/HOT_ChatAuthController.setStatusCompleted';
+
+export default class AuthenticationCompletedHandler extends LightningElement {
+    connectedCallback() {
+        const chatTranscriptId = this.getUrlParamValue(window.location.href, 'ctid');
+
+        setStatusCompleted({ chatTranscriptId: chatTranscriptId })
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        // eslint-disable-next-line @lwc/lwc/no-async-operation, @locker/locker/distorted-window-set-timeout
+        setTimeout(function () {
+            window.close();
+        }, 5000);
+    }
+
+    getUrlParamValue(url, key) {
+        // eslint-disable-next-line compat/compat
+        return new URL(url).searchParams.get(key);
+    }
+}
