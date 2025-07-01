14477e070ac8d2ed715d15e39e3c5da2b4f7903d
Eugenijus <eugenijus.margalikas@nav.no>
2025-07-01 15:13:49 +0200

    update hot_buttoncontainerBottom

--- force-app/main/default/lwc/hot_buttonContainerBottom/hot_buttonContainerBottom.js
+++ force-app/main/default/lwc/hot_buttonContainerBottom/hot_buttonContainerBottom.js
@@ -102,16 +102,19 @@ export default class HotButtonContainerBottom extends LightningElement {
     }
 
     updateFlowLoop() {
+        let baseClasses = 'slds-button slds-button_stretch button-spacing button-heights ';
         this.flowLoop = this.flowNameList?.map((flowName, index) => ({
             developerName: flowName,
             label: this.labelList ? this.labelList[index] : flowName,
             expanded: (this.activeFlow === flowName).toString(),
-            buttonStyling: this.buttonStylingList.length ? this.buttonStylingList[index] : 'secondary'
+            buttonStyling:
+                baseClasses +
+                (this.buttonStylingList.length ? this.buttonStylingList[index] : 'slds-button_outline-brand')
         }));
     }
 
     toggleFlow(event) {
-        const flowName = event.detail?.dataId;
+        const flowName = event.currentTarget?.dataset?.id; //event.detail?.dataId;
         if (!flowName) return;
         if (this.activeFlow === flowName) {
             this.activeFlow = '';
